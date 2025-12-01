aa09085eff4e7d886fb5cff8364343b301c3dba3
Kortnee Rong <kortneer@nycstudents.net>
2025-12-01 09:32:21 -0800

    m nh n n n n

--- music-playlist/src/main.js
+++ music-playlist/src/main.js
@@ -1,2 +1,75 @@
 import './style.css'
+const songs = [
+    {
+        name: "Dangerous",
+        artist: "VOLT VISION, Beneath My Shade",
+        image:"dangerous.jpg",
+        alt: "dangerous"
+
+    },
+        {
+        name: "MY CLEMATIS",
+        artist: "Rubyeye, C!naH",
+        image:"clematis.jpg",
+        alt:"clematis",
+
+    },
+        {
+        name: "Unknown (Till the End...)",
+        artist: "AKUGETSU",
+        image:"unknown.jpg",
+        alt:"unknown",
+
+    },
+        {
+        name: "Black Sorrow",
+        artist: "PARK BYEONG HOON",
+        image:"blacksorrow.jpg",
+        alt:"blacksorrow",
+
+    },
+        {
+        name: "Ruler of my Heart",
+        artist: "BL8M, Rubyeye",
+        image:"rulerofmyheart.jpg",
+        alt:"rulerofmyheart",
+
+    },
+        {
+        name: "All-In",
+        artist: "6FU;",
+        image:"allin.webp",
+        alt:"allin",
+
+    },
+        {
+        name: "Cure",
+        artist: "AKUGETSU, PARK BYEONG HOON",
+        image:"cure.jpg",
+        alt:"cure",
+
+    },
+        {
+        name: "Blink Gone",
+        artist: "BL8M, AKUGETSU",
+        image:"blinkgone.jpg",
+        alt: "blinkgone",
+
+    },
+        {
+        name: "Wiege",
+        artist: "BL8M, 6FU;",
+        image:"wiege.jpg",
+        alt:"weige",
+
+    },
+        {
+        name: "KARMA",
+        artist: "Rubyeye, C!naH",
+        image:"karma.jpg",
+        alt:"karma",
+
+    }
+    
+]
 
